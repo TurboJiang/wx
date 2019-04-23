8a056378b246665a782d6b950d29cf021702f52a
Jiangyf <2212761114@qq.com>
2019-04-23 21:25:57 +0800

    wxapp

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,37 +1,27 @@
 Page({
   data: {
-    //   imgUrls: [
-    //     'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
-    //     'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
-    //     'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
-    //   ],
-    //   indicatorDots: false,
-    //   autoplay: false,
-    //   interval: 5000,
-    //   duration: 1000
-    // },
-    // changeIndicatorDots(e) {
-    //   this.setData({
-    //     indicatorDots: !this.data.indicatorDots
-    //   })
-    // },
-    // changeAutoplay(e) {
-    //   this.setData({
-    //     autoplay: !this.data.autoplay
-    //   })
-    // },
-    // intervalChange(e) {
-    //   this.setData({
-    //     interval: e.detail.value
-    //   })
-    // },
-    // durationChange(e) {
-    //   this.setData({
-    //     duration: e.detail.value
-    //   })
-    // }
-
-    city:"南昌"
-
+    city: "南昌",
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    items: []
+  },
+  onLoad() {
+    let that = this//以防出现this作用域被覆盖 所有的this均指向数据源(data)
+    // 小程序请求接口数据
+    wx.request({
+      url:'https://www.easy-mock.com/mock/5cbeb53158b92c6ed8f6f90a/movieWxapp/movie-WXAPP',
+      data:{},//currentPage...传参给后端
+      method:'GET',
+      header:{'Content-type':'application/json'},//请求头
+      success:function(res){
+        console.log(res);
+        that.setData({//设置数据
+          items:res.data.data.movieList//赋值用冒号
+        })
+      }
+    })
   }
 })
